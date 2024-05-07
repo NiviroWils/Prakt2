740609bbaa4db00dab5a4e432031e023b305bfd9
NiviroWils <ksu280705@yandex.ru>
2024-05-07 09:04:27 +0700

    Окончание работы над тестами регистрации(2)

--- cypress/e2e/reg_negative.cy.js
+++ cypress/e2e/reg_negative.cy.js
@@ -1,12 +1,12 @@
 describe('template spec', () => {
     it('passes', () => {
-      cy.visit('https://dev.profteam.su/registration')
+        cy.visit('https://dev.profteam.su/registration')
     })
     it('None-existent password test', () => {
         cy.fixture('cypressTests').then(data => {
             cy.log('Положительный тест-кейс регистрации')
             cy.visit('https://dev.profteam.su/registration')
-        //Тест-кейс для проверки регистрации (положительный)
+            //Тест-кейс для проверки регистрации (негативный)
             cy.log('Ввод предположительно несуществующего логина')
             cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text')
                 .type(data.test_login)
@@ -30,30 +30,8 @@ describe('template spec', () => {
 
             cy.wait(3000)
 
-            cy.log('Ввод фамилии')
-            cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text')
-                .type(data.test_surname)
 
-            cy.log('Ввод имени')
-            cy.get(':nth-child(2) > .form-control--medium > .form-input--text')
-                .type(data.test_name)
-
-            cy.log('Ввод отчества')
-            cy.get(':nth-child(3) > .form-control--medium > .form-input--text')
-                .type(data.test_patronymic)
-
-            cy.log('Клик по кнопке "Создать аккаунт"')
-            cy.get(':nth-child(3) > .button')
-                .click()
-
-            cy.wait(3000)
-
-            cy.log('Клик по кнопке "Выйти"')
-            cy.get('[data-v-7ff322b7=""][data-v-f5840952=""] > .button')
-                .click()
-
-            cy.wait(5000)
 
         })
     })
-  })
+})
