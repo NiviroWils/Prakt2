f8d4c4714aa7a2dbf475325ff4122fc9dac0648e
NiviroWils <ksu280705@yandex.ru>
2024-05-07 08:36:47 +0700

    Окончание работы над тестами регистрации

--- cypress/e2e/reg.cy.js
+++ cypress/e2e/reg.cy.js
@@ -1,11 +1,10 @@
 describe('template spec', () => {
     it('passes', () => {
-      cy.visit('https://dev.profteam.su/login')
+      cy.visit('https://dev.profteam.su/registration')
     })
     it('None-existent password test', () => {
-        //Тест-кейс для проверки некорректного пароля
-
-            cy.log('Ввод несуществующего логина')
+        //Тест-кейс для проверки регистрации (положительный)
+            cy.log('Ввод предположительно несуществующего логина')
             cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text')
                 .type(data.test_login)
 
@@ -17,45 +16,38 @@ describe('template spec', () => {
             cy.get(':nth-child(3) > .form-control--medium > .form-input--password')
                 .type(data.test_passwd)
 
-            cy.log('Клик по кнопке "Войти"')
-            cy.get(':nth-child(3) > .button')
+            cy.log('Ввод повторный ввод пароля')
+            cy.get(':nth-child(4) > .form-control--medium > .form-input--password')
+                .type(data.test_passwd)
+
+            cy.log('Клик по кнопке "Далее"')
+            cy.get('.form__buttons > :nth-child(4)')
                 .click()
-            
-            
-            cy.get('.form-error')
-                .should('exist')
+
 
             cy.wait(5000)
 
-            //Тест-кейс для проверки заполнения полей
-            cy.log('Очистка поля "Логин"')
-            cy.get('.form-input--text')
-                .clear()
+        //Тест-кейс для проверки регистрации (отрицательный)
 
-            cy.log('Очистка поля "Пароль"')
-            cy.get('.form-input--password')
-                .clear()
-            cy.get(':nth-child(1) > .form-error > span')
-                .should('exist')
+            cy.log('Ввод предположительно несуществующего логина')
+            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text')
+                .type(data.test_login)
 
-            cy.wait(5000)
+            cy.log('Ввод email')
+            cy.get('.form-input--email')
+                .type(data.example_email)
 
-        //Тест-кейс для проверки некорректного логина
-            cy.log('Ввод несуществующего логина')
-            cy.get('.form-input--text')
-                .type(data.none_existent_login)
+            cy.log('Ввод пароля')
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--password')
+                .type(data.test_passwd)
 
-            cy.log('Ввод существующего пароля')
-            cy.get('.form-input--password')
-                .type(data.existent_passwd)
-    
+            cy.log('Ввод повторный ввод пароля')
+            cy.get(':nth-child(4) > .form-control--medium > .form-input--password')
+                .type(data.test_passwd)
 
-            cy.log('Клик по кнопке "Войти"')
-            cy.get(':nth-child(3) > .button')
+            cy.log('Клик по кнопке "Далее"')
+            cy.get('.form__buttons > :nth-child(4)')
                 .click()
-            
-            cy.get('.form-error')
-                .should('exist')
 
         })
   })
