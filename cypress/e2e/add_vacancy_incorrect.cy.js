246e39565a23dc01b9ff0ff0c546d71b6f7dcb73
NiviroWils <ksu280705@yandex.ru>
2024-05-08 09:13:21 +0700

    Создание негативного тест-кейса для создания новой вакансии и валидации логина при регистрации

---
+++ cypress/e2e/add_vacancy_incorrect.cy.js
@@ -0,0 +1,95 @@
+describe('template spec', () => {
+    it('passes', () => {
+        cy.visit('https://dev.profteam.su/login')
+    })
+    //Тест-кейс для корректных данных входа
+    it('Existent login test', () => {
+        cy.fixture('cypressTests').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Ввод существующего логина')
+            cy.get('.form-input--text')
+                .type(data.login_employer)
+
+            cy.log('Ввод существующего пароля')
+            cy.get('.form-input--password')
+                .type(data.existent_passwd)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get(':nth-child(3) > .button')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Клик по кнопке "Вакансии"')
+            cy.get(':nth-child(7) > .menu-item__item-name')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Клик по кнопке "Создать вакансию"')
+            cy.get('[data-v-94414c9f=""][data-v-4849dea2=""] > .vacancies-block > .vacancies-block__filters-wrapper > .button')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Ввод названия вакансии')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--')
+                .type(data.incorrect_vacancy_name, {force: true})
+
+            cy.wait(2000)
+
+            cy.log('Выбор типа заработной платы')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(2) > .salary-field > .salary-field__wrapper--bottom > .radio-list > :nth-child(3) > .radio-component__input')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Ввод размера заработной платы')
+            cy.get('.form-input--number')
+                .type(data.vacancy_salary)
+
+            cy.wait(2000)
+
+            cy.log('Выбор типа занятости')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(3) > .form-select > :nth-child(2) > .form-select__selected')
+                .click()
+
+            cy.wait(2000)
+
+            cy.get('.form-select__items > :nth-child(1)')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Выбор графика работы')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(4) > [data-v-af677f15=""] > :nth-child(1) > .radio-list > :nth-child(2) > .radio-component__input')
+                .click()
+
+            cy.wait(2000)
+
+            cy.log('Ввод требований')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(5) > .form-control > .form-area')
+                .type(data.vacancy_requirements)
+
+            cy.wait(2000)
+
+            cy.log('Ввод обязанностей')
+            cy.get('.vacancy-add-form-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(6) > .form-control > .form-area')
+                .type(data.vacancy_duties)
+
+            cy.wait(2000)
+
+            cy.log('Клик по кнопке "Обновить вакансию"')
+            cy.get('.vacancy-add-form-wrapper > .form > .form__buttons > .buttons > .button')
+                .click({force: true})
+
+            cy.wait(2000)
+
+
+
+
+        })
+    } )
+})
---
+++ cypress/e2e/reg_incorrect_login2.cy.js
@@ -0,0 +1,30 @@
+describe('template spec', () => {
+    it('passes', () => {
+        cy.visit('https://dev.profteam.su/registration')
+    })
+    it('None-existent password test', () => {
+        cy.fixture('cypressTests').then(data => {
+            cy.log('Положительный тест-кейс регистрации')
+            cy.visit('https://dev.profteam.su/registration')
+            //Тест-кейс для проверки регистрации (положительный)
+            cy.log('Ввод Невалидного логина')
+            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text')
+                .type(data.incorrect_login2)
+
+            cy.log('Ввод  email')
+            cy.get('.form-input--email')
+                .type(data.example_email)
+
+            cy.log('Ввод пароля')
+            cy.get(':nth-child(3) > .form-control--medium > .form-input--password')
+                .type(data.test_passwd)
+
+            cy.log('Повторный ввод пароля')
+            cy.get(':nth-child(4) > .form-control--medium > .form-input--password')
+                .type(data.test_passwd)
+
+
+
+        })
+    })
+})
